293b9e8816c8a9e7cede5c573852ef33bf454852
charbo23 <charbo233@qq.com>
2019-09-03 18:47:59 +0800

    bugfix

--- react-hooks-demo01/src/store/reducer.js
+++ react-hooks-demo01/src/store/reducer.js
@@ -10,51 +10,21 @@ const defaultState = Map({
 export default (state = defaultState, action) => {
     //Reducer里只能接收state不能改变state
     if (action.type === types.INIT_LIST) {
-        // let newState = _.cloneDeep(state);
-        // newState.loading = true;
-        // return newState;
         return state.set('loading', true);
     }
     if (action.type === types.GET_LIST) {
-        // let newState = _.cloneDeep(state);
-        // newState.list = action.list;
-        // newState.loading = false;
-        // return newState;
         return state.merge({
             list: List(action.list),
             loading: false
         })
     }
     if (action.type === types.CHANGE_INPUT) {
-        // let newState = _.cloneDeep(state);
-        // newState.inputValue = action.value;
-        // return newState;
         return state.set('inputValue', action.value);
     }
 
     if (action.type === types.ADD_ITEM) {
-        // let newState = _.cloneDeep(state);
-        // let listItemValue = action.value || newState.inputValue;
-        // if (listItemValue.trim()) {
-        //     newState.list.push(
-        //         {
-        //             id: shortid.generate(),
-        //             value: listItemValue
-        //         }
-        //     );
-        // }
-        // newState.inputValue = '';
-        // return newState;
         let listItemValue = action.value || state.get('inputValue');
         if (listItemValue.trim()) {
-            // return state.update('list',(list)=>{
-            //     return list.push(
-            //         {
-            //             id: shortid.generate(),
-            //             value: listItemValue
-            //         }
-            //     );
-            // }).set('inputValue','');
             return state.merge({
                 list: state.get('list').push({
                     id: shortid.generate(),
@@ -64,14 +34,8 @@ export default (state = defaultState, action) => {
             })
         }
         return state.set('inputValue', '');
-
     }
     if (action.type === types.DELETE_ITEM) {
-        // let newState = _.cloneDeep(state);
-        // newState.list = newState.list.filter((listItem) => {
-        //     return listItem.id !== action.itemId
-        // });
-        // return newState;
         return state.update('list', (list) => {
             return list.filter((listItem) => {
                 return listItem.id !== action.itemId
@@ -79,9 +43,6 @@ export default (state = defaultState, action) => {
         })
     }
     if (action.type === types.ERROR) {
-        // let newState = _.cloneDeep(state);
-        // newState.loading = false;
-        // return newState;
         return state.set('loading', false);
     }
     return state;
