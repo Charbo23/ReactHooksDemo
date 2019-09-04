81ce65450e3d9ceb2ae3eb40ce05323a595f19a2
charbo23 <charbo233@qq.com>
2019-09-04 21:37:50 +0800

    优化前

---
+++ react-hooks-demo01/src/Example7.js
@@ -0,0 +1,44 @@
+import React, { useState, Fragment } from 'react';
+import { Button } from 'antd';
+import '@/style/antd-style.module.scss';
+
+function Example7() {
+    const [xiaohong, setXiaohong] = useState('小红在等待');
+    const [zhiling, setZhiling] = useState('小林在等待');
+    return (
+        <div>
+            <Button
+                type='danger'
+                style={{ backgroundColor: '#ff4d4f', borderColor: '#ff4d4f' }}
+                onClick={() => setXiaohong(new Date().getTime())}
+            >
+                小红
+            </Button>
+            <Button
+                type='primary'
+                style={{ backgroundColor: '#faad14', borderColor: '#faad14' }}
+                onClick={() => setZhiling(new Date().getTime() + '，小林向我们走来了')}
+
+            >
+                小林
+            </Button>
+            <ChildComponent name={xiaohong}>{zhiling}</ChildComponent>
+        </div>
+    )
+}
+export default Example7;
+
+function ChildComponent({ name, children }) {
+    function changeXiaohong() {
+        //点击小林按钮依然会调用，存在性能问题
+        console.log('他来了他来了');
+        return name + '，小红来了';
+    }
+    const actionXiaohong = changeXiaohong(name)
+    return (
+        <Fragment>
+            <h2>{actionXiaohong}</h2>
+            <h2>{children}</h2>
+        </Fragment>
+    )
+}
--- react-hooks-demo01/src/index.js
+++ react-hooks-demo01/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-import Example6 from './example6/Example6';
+import Example7 from './Example7';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Example6/>, document.getElementById('root'));
+ReactDOM.render(<Example7/>, document.getElementById('root'));
 
 //可让网站像一个应用一样离线访问，但只有在localhost或https协议下才有效
 serviceWorker.register();
