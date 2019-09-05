3522d26911b32fa6df5df6c9d5c32a08bece8397
charbo23 <charbo233@qq.com>
2019-09-05 15:45:17 +0800

    bugfix

--- react-hooks-demo01/src/Example9.js
+++ react-hooks-demo01/src/Example9.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import _ from 'lodash';
 function useWinSize() {
     const [size, setSize] = useState({
