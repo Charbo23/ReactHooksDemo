12df341b4b1c2d324ed94c225bfc5ddb637d5f8a
charbo23 <charbo233@qq.com>
2019-09-07 12:48:34 +0800

    学习Promise结合Hooks

--- react-hooks-demo01/src/Example8.js
+++ react-hooks-demo01/src/Example8.js
@@ -2,10 +2,11 @@ import React, { Fragment, useState, useRef, useEffect } from 'react';
 import { Button, Input } from 'antd';
 import antdStyle from './style/antd-style.module.scss';
 import useWinSize from './Example9';
-
+import { List } from 'immutable';
 function Example8() {
     const [title, setTitle] = useState('Title');
     const [input, setInput] = useState('');
+    const [list, setList] = useState(List([]));
     const inputRef = useRef();
     const size = useWinSize();
     const handleClick = () => {
@@ -18,9 +19,13 @@ function Example8() {
     const changeInput = (e) => {
         setInput(e.target.value)
     }
+    const fetchList = ({ page = 1, size = 10 }) =>
+        new Promise(resolve => resolve(list => list.push('1', '2')));
+    //原生数组的push方法返回的是长度不是新数组！！大坑
     useEffect(() => {
         inputRef.current.inputValue = input;
-    },[input])//只有当input改变才调用
+        fetchList({}).then(setList);
+    }, [input])//只有当input改变才调用
     return (
         <Fragment>
             <h1>{title}</h1>
@@ -41,6 +46,9 @@ function Example8() {
             </Button>
             <h2>{'Width:' + size.width}</h2>
             <h2>{'Height:' + size.height}</h2>
+            <h2>{list.map((item, index) => {
+                return index === 0 ? item : ',' + item;
+            })}</h2>
         </Fragment>
     )
 }
